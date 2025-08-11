ef8db5ae95ce291942d090ceb3f5119b8579fe4e
Harun Ibram <harun.ibram@outlook.com>
2025-08-11 13:25:30 +0300

    Create TCP Server and Client interfaces

--- Harmony/mainwindow.cpp
+++ Harmony/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include <QDebug>
 #include <QTcpSocket>
 #include <QInputEvent>
+#include <QTcpServer>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "connectdialog.h"
@@ -13,6 +15,15 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::onSendButtonClicked);
     connect(ui->actionConnect, &QAction::triggered, this, &MainWindow::onConnectClicked);
 
+    server = new QTcpServer(this);
+    server = new QTcpServer(this);
+    connect(server, &QTcpServer::newConnection, this, &MainWindow::onNewConnection);
+
+    if (!server->listen(QHostAddress::Any, 5002)) {
+        qDebug() << "Server failed to start:" << server->errorString();
+    } else {
+        qDebug() << "Server listening on port" << server->serverPort();
+    }
 
     ui->messageDisplay->setIconSize(QSize(48, 48));
     ui->messageEdit->installEventFilter(this);
@@ -24,17 +35,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::onSendButtonClicked() {
     QString message = ui->messageEdit->toPlainText();
     if (!message.isEmpty()) {
         QListWidgetItem *item = new QListWidgetItem("Art\n" + message);
+
         QPixmap avatarImage(":/assets/assets/small_cassie.png");
+
         item->setIcon(QIcon(avatarImage));
+
         ui->messageDisplay->addItem(item);
+
         ui->messageDisplay->scrollToBottom();
+
         ui->messageEdit->clear();
     }
+}
+
+void MainWindow::connectSocket(QTcpSocket *socket, QString ip, quint16 port) {
+    socket->connectToHost(ip, port);
 }
 
 void MainWindow::onConnectClicked() {
@@ -44,9 +63,11 @@ void MainWindow::onConnectClicked() {
         quint16 port = dialog.getPort();
 
 
-        QTcpSocket *socket = new QTcpSocket(this);
-        socket->connectToHost(ip, port);
+        socket = new QTcpSocket(this);
+        connectSocket(socket, ip, port);
+
         ui->messageDisplay->clear();
+
         ui->messageDisplay->addItem("Connected to " + ip + " : " + port);
     }
 }
@@ -61,3 +82,33 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
     }
     return QMainWindow::eventFilter(obj, event);
 }
+
+void MainWindow::onNewConnection() {
+    if (QMessageBox::question(
+            this,
+            "Incoming Connection",
+            "A client is trying to connect.\nDo you want to accept?",
+            QMessageBox::Yes | QMessageBox::No
+            ) == QMessageBox::Yes)
+    {
+        QTcpSocket *clientSocket = server->nextPendingConnection();
+        qDebug() << "Client connected from" << clientSocket->peerAddress().toString();
+
+        connect(clientSocket, &QTcpSocket::readyRead, this, &MainWindow::onReadyRead);
+        connect(clientSocket, &QTcpSocket::disconnected, clientSocket, &QTcpSocket::deleteLater);
+    }
+    else {
+        // Refuse the connection: just discard the pending connection
+        QTcpSocket *temp = server->nextPendingConnection();
+        temp->disconnectFromHost();
+        temp->deleteLater();
+    }
+}
+
+void MainWindow::onReadyRead() {
+
+}
+
+
+
+
--- Harmony/mainwindow.h
+++ Harmony/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QTcpSocket>
+#include <QTcpServer>
 #include "ui_mainwindow.h"
 
 
@@ -23,8 +25,15 @@ protected:
     bool eventFilter(QObject *obj, QEvent *event) override;
 
 private:
+    QTcpSocket *socket = nullptr;
+    QTcpServer *server = nullptr;
+    QTcpSocket *clientSocket = nullptr;
     Ui::MainWindow *ui;
     void onSendButtonClicked();
     void onConnectClicked();
+    void connectSocket(QTcpSocket *socket, QString ip, quint16 port);
+    void onNewConnection();
+    void onReadyRead();
+    QTcpSocket *startServer(quint16 port);
 };
 #endif // MAINWINDOW_H
